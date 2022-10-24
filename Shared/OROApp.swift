ded603c73b12e39dbcbbae4a09f53db1f0092509
bestmycode <bestmycode610@gmail.com>
2022-10-25 02:11:55 +0800

    init app

---
+++ Shared/OROApp.swift
@@ -0,0 +1,17 @@
+//
+//  OROApp.swift
+//  Shared
+//
+//  Created by mac on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
