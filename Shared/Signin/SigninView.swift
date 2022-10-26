09bd0af17bca03f7becd6dd64fc4bb06edc16b6e
bestmycode <bestmycode610@gmail.com>
2022-10-26 05:19:30 -0300

    add signin screen

---
+++ Shared/Signin/SigninView.swift
@@ -0,0 +1,55 @@
+//
+//  SigninView.swift
+//  Shared
+//
+//  Created by mac on 2022/10/25.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    let background = LinearGradient(gradient: Gradient(colors: [Color(red: 86/255, green: 183/255, blue: 230/255), Color(red: 121/255, green: 211/255, blue: 255/255)]), startPoint: .top, endPoint: .bottom);
+    let containerWidth:CGFloat = UIScreen.main.bounds.width;
+    let containerHeight:CGFloat = UIScreen.main.bounds.height;
+    var body: some View {
+        ZStack {
+            background
+            .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .center)
+            {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 0.55 * containerWidth)
+                HStack(alignment: .center)
+                {
+                    Image("Letter_o")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 0.183 * containerWidth)
+                    Image("Letter_r")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 0.13 * containerWidth)
+                    Image("Letter_o")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 0.183 * containerWidth)
+                }
+                .padding(EdgeInsets(top: 0.0446 * containerHeight, leading: 0, bottom: 0, trailing: 0))
+                Text("Speak Freely")
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 0.16 * containerHeight, leading: 0, bottom: 0, trailing: 0))
+            }
+            .background(background)
+            .scaledToFill()
+        }
+    }
+}
+
+struct SigninView_Previews: PreviewProvider {
+    static var previews: some View {
+        SigninView()
+    }
+}
--- Shared/Splash/SplashView.swift
+++ Shared/Splash/SplashView.swift
@@ -8,42 +8,59 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var isActive = false
+    
     let background = LinearGradient(gradient: Gradient(colors: [Color(red: 86/255, green: 183/255, blue: 230/255), Color(red: 121/255, green: 211/255, blue: 255/255)]), startPoint: .top, endPoint: .bottom);
     let containerWidth:CGFloat = UIScreen.main.bounds.width;
     let containerHeight:CGFloat = UIScreen.main.bounds.height;
+
     var body: some View {
-        ZStack {
-            background
-            .edgesIgnoringSafeArea(.all)
-            VStack(alignment: .center)
-            {
-                Image("Logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 0.55 * containerWidth)
-                HStack(alignment: .center)
+        NavigationView {
+            ZStack {
+                background
+                .edgesIgnoringSafeArea(.all)
+                VStack(alignment: .center)
                 {
-                    Image("Letter_o")
+                    Image("Logo")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 0.183 * containerWidth)
-                    Image("Letter_r")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 0.13 * containerWidth)
-                    Image("Letter_o")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 0.183 * containerWidth)
+                        .frame(width: 0.55 * containerWidth)
+                    HStack(alignment: .center)
+                    {
+                        Image("Letter_o")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 0.183 * containerWidth)
+                        Image("Letter_r")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 0.13 * containerWidth)
+                        Image("Letter_o")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 0.183 * containerWidth)
+                    }
+                    .padding(EdgeInsets(top: 0.0446 * containerHeight, leading: 0, bottom: 0, trailing: 0))
+                    Text("Speak Freely")
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 0.16 * containerHeight, leading: 0, bottom: 0, trailing: 0))
+                    NavigationLink(destination: login,
+                        isActive: $isActive,
+                        label: { SigninView() })
                 }
-                .padding(EdgeInsets(top: 0.0446 * containerHeight, leading: 0, bottom: 0, trailing: 0))
-                Text("Speak Freely")
-                    .font(.system(size: 20))
-                    .foregroundColor(.white)
-                    .padding(EdgeInsets(top: 0.16 * containerHeight, leading: 0, bottom: 0, trailing: 0))
+                .background(background)
+                .scaledToFill()
             }
-            .background(background)
-            .scaledToFill()
+            .onAppear(perform: {
+                self.gotoLoginScreen(time: 3)
+            })
+        }
+    }
+
+    func gotoLoginScreen(time: Double) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + Double(time)) {
+            self.isActive = true
         }
     }
 }
