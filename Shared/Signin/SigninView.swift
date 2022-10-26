e2955552f264931a758eed16a4da83e318eda196
bestmycode <bestmycode610@gmail.com>
2022-10-26 18:15:34 +0800

    update signin page

--- Shared/Signin/SigninView.swift
+++ Shared/Signin/SigninView.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct SigninView: View {
+
+    let background = LinearGradient(gradient: Gradient(colors: [Color(red: 86/255, green: 183/255, blue: 230/255), Color(red: 121/255, green: 211/255, blue: 255/255)]), startPoint: .top, endPoint: .bottom)
+    let containerWidth:CGFloat = UIScreen.main.bounds.width
+    let containerHeight:CGFloat = UIScreen.main.bounds.height
+
     var body: some View {
-        Text("Signin")
+        NavigationView {
+            ZStack {
+                background
+                .edgesIgnoringSafeArea(.all)
+                VStack(alignment: .center)
+                {
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 0.352 * containerWidth)
+                    HStack(alignment: .center)
+                    {
+                        Image("Letter_o")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 0.096 * containerWidth)
+                        Image("Letter_r")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 0.065 * containerWidth)
+                        Image("Letter_o")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 0.096 * containerWidth)
+                    }
+                    .padding(EdgeInsets(top: 0.022 * containerHeight, leading: 0, bottom: 0, trailing: 0))
+                    Spacer()
+                    Button(action: {
+                        print("sign up bin tapped")
+                    }) {
+                        Text("Continue with Email")
+                            .frame(minWidth: 0, maxWidth: 0.8 * containerWidth)
+                            .font(.system(size: 18))
+                            .padding()
+                            .foregroundColor(.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.white, lineWidth: 2)
+                        )
+                    }
+                    .background(.black.opacity(0))
+                    .cornerRadius(8)
+                   
+                }
+                .background(background)
+                .scaledToFill()
+                .padding(EdgeInsets(top: 0.1 * containerHeight, leading: 0, bottom: 0.2 * containerHeight, trailing: 0))
+            }
+        }
     }
 }
 
