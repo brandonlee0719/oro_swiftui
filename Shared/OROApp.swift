c3b90f9800fa95e735c12176b7f75d0a0c78ddee
bestmycode <bestmycode610@gmail.com>
2022-10-26 05:03:51 -0300

    change splashview

--- Shared/OROApp.swift
+++ Shared/OROApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct OROApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
         }
     }
 }
--- Shared/Splash/SplashView.swift
+++ Shared/Splash/SplashView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SplashView.swift
 //  Shared
 //
 //  Created by mac on 2022/10/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SplashView: View {
     let background = LinearGradient(gradient: Gradient(colors: [Color(red: 86/255, green: 183/255, blue: 230/255), Color(red: 121/255, green: 211/255, blue: 255/255)]), startPoint: .top, endPoint: .bottom);
     let containerWidth:CGFloat = UIScreen.main.bounds.width;
     let containerHeight:CGFloat = UIScreen.main.bounds.height;
@@ -49,8 +49,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SplashView()
     }
 }
